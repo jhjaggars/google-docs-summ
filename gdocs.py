b7dec05ec8adfca436ba963308d53c675dd3c885
Jesse Jaggars <jhjaggars@gmail.com>
2024-04-15 08:54:30 -0400

    factor out auth

---
+++ gdocs.py
@@ -0,0 +1,34 @@
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+import os
+
+def get_credentials():
+    # Step 1: Set up the OAuth 2.0 flow for authentication
+    SCOPES = ['https://www.googleapis.com/auth/documents.readonly', 'https://www.googleapis.com/auth/drive.metadata.readonly']
+    creds = None
+    token_file = 'token.json'
+    credentials_file = 'credentials.json'
+
+    if os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_file, 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def get_docs_service(creds=None):
+    
+    if creds is None:
+        creds = get_credentials()
+
+    # Step 2: Build the service objects
+    docs_service = build('docs', 'v1', credentials=creds)
+    return docs_service
--- to_obsidian.py
+++ to_obsidian.py
@@ -1,36 +1,10 @@
 # given a document id, produce a valid obsidian markdown file with a summary as well as a link to the full document
-
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
 import os
 import argparse
 import time
 import sys
 from langchain_community.llms import Ollama
-
-def get_docs_service():
-    # Step 1: Set up the OAuth 2.0 flow for authentication
-    SCOPES = ['https://www.googleapis.com/auth/documents.readonly', 'https://www.googleapis.com/auth/drive.metadata.readonly']
-    creds = None
-    token_file = 'token.json'
-    credentials_file = 'credentials.json'
-
-    if os.path.exists(token_file):
-        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open(token_file, 'w') as token:
-            token.write(creds.to_json())
-
-    # Step 2: Build the service objects
-    docs_service = build('docs', 'v1', credentials=creds)
-    return docs_service
+from gdocs import get_docs_service
 
 def parse_doc(doc):
     doc_title = doc.get('title')
